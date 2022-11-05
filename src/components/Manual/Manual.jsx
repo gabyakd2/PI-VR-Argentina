3a6ca2c0f59ce75fe16867ddfea2e3fe4e4b9133
gabyakd2 <gabrielsal00@hotmai.com>
2022-11-05 19:14:26 -0300

    ruteo manual

---
+++ src/components/Manual/Manual.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../Navbar/Navbar";
+
+
+export default function Manual() {
+    return(
+        <div>
+            <NavBar />
+            <h1>SOY EL MANUAL</h1>
+        </div>
+    )
+}
--- src/routes/RouterController.jsx
+++ src/routes/RouterController.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../components/Home/Home';
 import Faqs from '../components/Faqs/Faqs';
+import Manual from '../components/Manual/Manual';
 
 function RouteController() {
     return (
@@ -8,6 +9,7 @@ function RouteController() {
             <Routes>
                 <Route exact path='/home' element={<Home />} />
                 <Route path='/faqs' element={<Faqs />} />
+                <Route path='/usermanual' element={<Manual />} />
             </Routes>
         </div>
     )
