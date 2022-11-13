bc90d5b0eac7dcf7ac60b4e6156772b4b5eaf6b3
gabyakd2 <gabrielsal00@hotmai.com>
2022-11-13 19:05:27 -0300

    home, faqs y contactos retocados. Comienzo de manual

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,13 +4,13 @@ import './contact.css'
 
 export default function Contact() {
     return (
-        <div>
+        <div className="dipricontact">
             <div className="containerPri">
                 <NavBar />
-                <div className="container-sm mt-5 bg-dark" id="divContenedor">
-                    <h2 className="h1contact">Para obtener más información o adquirir tu Pi-Vr</h2>
-                    <h3 className="subtitlecontact mt-5">Contactanos por nuestras redes</h3>
-                    <div className="container mt-4" id="logoFace">
+                <div className="container-sm mt-5 bg-dark" id="divContenedorContact">
+                    {/* <h2 className="h1contact">Para obtener más información o adquirir tu Pi-Vr</h2> */}
+                    <h3 className="subtitlecontact pt-5">Contactanos por nuestras redes</h3>
+                    <div className="container mt-5" id="logoFace">
                         <a href="https://www.facebook.com/profile.php?id=100087346975844"><svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-facebook mx-5" id="logoimg" viewBox="0 0 16 16" >
                             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                         </svg></a>
--- src/components/Manual/Manual.jsx
+++ src/components/Manual/Manual.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import NavBar from "../Navbar/Navbar";
+import './manual.css'
 
 
 export default function Manual() {
     return(
         <div>
-            <NavBar />
-            <h1>SOY EL MANUAL</h1>
+            <div className="containerPriFaqs">
+                <NavBar />
+                <div className="container bg-dark mt-5" id="divContenedorFaqs">
+                    <h1 className="h1manual">Manual de usuario</h1>
+                    
+                </div>
+            </div>
         </div>
     )
 }
