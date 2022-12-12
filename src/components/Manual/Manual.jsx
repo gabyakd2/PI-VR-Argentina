03d1f0f5b35cec1f5c36a6140a16616490b3dc25
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2022-12-12 10:23:35 -0300

    css

--- src/components/Manual/Manual.jsx
+++ src/components/Manual/Manual.jsx
@@ -1,37 +1,111 @@
 import React from "react";
 import NavBar from "../Navbar/Navbar";
 import "./manual.css";
+import { Link } from "react-router-dom";
+// import hamburguesa from "../../imagenes/hamburguesa.png";
 
 export default function Manual() {
   return (
-    <div>
-      <div className="containerPriFaqs">
-        <NavBar />
-        <div className="container bg-dark mt-5" id="divContenedorFaqs">
-          <h1 className="h1manual">Manual de usuario</h1>
-          <input type="checkbox" className="checkbox" id="check" />
-      <label className="menu" htmlFor="check">
-        |||
-      </label>
-      <div className="left-panel">
-        <ul>
-          <a href="#">
-            <li>Introducción</li>
-          </a>
-          <a href="#">
-            <li>Programas necesarios</li>
-          </a>
-          <a href="#">
-            <li>Primeros pasos</li>
-          </a>
-          <a href="#">
-            <li>Configuracion de SteamVR</li>
-          </a>
-        </ul>
-      </div>
+    <div className="containerPriFaqs">
+      <NavBar />
+      <div className="collapse" id="navbarToggleExternalContent">
+        <div className="bg-#212529 p-4">
+          <div className="container-fluid">
+            <ul class="nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" href="#">
+                  Introduccion
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="#">
+                  Programas Necesarios
+                </Link>
+              </li>
+              <li className="nav-item">
+                <button
+                  class="btn btn-secondary dropdown-toggle estilos"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Primeros Pasos
+                </button>
+                <ul class="dropdown-menu segundo">
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Configuracion de SteamVR
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Configuración del OVR Advanced Settings
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Colocación
+                    </Link>
+                    <li>
+                      <Link className="dropdown-item" href="#">
+                        Calibración
+                      </Link>
+                    </li>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="#">
+                  Problemas comunes
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+      <div className="container-sm mt-5 bg-dark" id="divContenedor"></div>
+      <div className="container-sm mt-5 bg-dark" id="divContenedor">
+        <h1 className="h1manual">Manual de usuario</h1>
+        <p className="parrafoHome">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, quos
+          officiis, dolorum unde, vel aspernatur saepe quia impedit ex libero
+          excepturi esse velit odit reiciendis voluptatum aperiam. Tenetur,
+          provident recusandae! Tempora consectetur vel, modi omnis laudantium
+          optio atque impedit, iste deserunt voluptatum beatae doloremque
+          corrupti at. Modi esse perferendis error consequuntur unde et, maxime,
+          doloribus nobis eligendi corrupti impedit velit. Iure quaerat
+          architecto atque unde deleniti nemo obcaecati quod, itaque quisquam.
+          Numquam sapiente nobis odit repudiandae fugit sed cum quis earum
+          aspernatur dolorum, qui officia asperiores autem dolore optio est!
+        </p>
+        <p className="parrafoHome">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, quos
+          officiis, dolorum unde, vel aspernatur saepe quia impedit ex libero
+          excepturi esse velit odit reiciendis voluptatum aperiam. Tenetur,
+          provident recusandae! Tempora consectetur vel, modi omnis laudantium
+          optio atque impedit, iste deserunt voluptatum beatae doloremque
+          corrupti at. Modi esse perferendis error consequuntur unde et, maxime,
+          doloribus nobis eligendi corrupti impedit velit. Iure quaerat
+          architecto atque unde deleniti nemo obcaecati quod, itaque quisquam.
+          Numquam sapiente nobis odit repudiandae fugit sed cum quis earum
+          aspernatur dolorum, qui officia asperiores autem dolore optio est!
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,58 @@
 import React from "react";
-import { Link } from 'react-router-dom'
-import logoPiVr from '../../imagenes/logoPiVr.png'
-import './navbar.css'
-
+import { Link } from "react-router-dom";
+import logoPiVr from "../../imagenes/logoPiVr.png";
+import "./navbar.css";
 
 export default function NavBar() {
-    return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-            <div className="container-fluid">
-                <Link to='/home' className="navbar-brand">
-                    <img className="logo" src={logoPiVr} alt="logopivr"/>
-                </Link>
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link to="/home" className="navbar-brand">
+          <img className="logo" src={logoPiVr} alt="logopivr" />
+        </Link>
 
-                <button className="navbar-toggler"
-                        type='button'
-                        data-bs-toggle='collapse'
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                <div className="collapse navbar-collapse" id='navbarSupportedContent'>
-                    <ul className="navbar-nav ms-auto px-4">
-                    <li className="nav-item"><Link to='/home' className="nav-link mx-4">HOME</Link></li>
-                    <div className="vr bg-light"></div>
-                    <li className="nav-item"><Link to='/usermanual' className="nav-link mx-4">MANUAL</Link></li>
-                    <div className="vr bg-light"></div>
-                    <li className="nav-item"><Link to='/faqs' className="nav-link mx-4">FAQ's</Link></li>
-                    <div className="vr bg-light"></div>
-                    <li className="nav-item"><Link to='/contact' className="nav-link mx-4">CONTACTO</Link></li>
-                    <div className="vr bg-light"></div>
-                    <li className="nav-item"><Link to='/nosotros' className="nav-link mx-4">NOSOTROS</Link></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto px-4">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link mx-4" id="letra">
+                HOME
+              </Link>
+            </li>
+            <div className="vr bg-light"></div>
+            <li className="nav-item">
+              <Link to="/usermanual" className="nav-link mx-4" id="letra">
+                MANUAL
+              </Link>
+            </li>
+            <div className="vr bg-light"></div>
+            <li className="nav-item">
+              <Link to="/faqs" className="nav-link mx-4" id="letra">
+                FAQ's
+              </Link>
+            </li>
+            <div className="vr bg-light"></div>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link mx-4" id="letra">
+                CONTACTO
+              </Link>
+            </li>
+            <div className="vr bg-light"></div>
+            <li className="nav-item"></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
