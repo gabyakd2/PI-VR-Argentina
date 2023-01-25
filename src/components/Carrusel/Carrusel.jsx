481bcd2e7c7cbba0a0aeb37f127978a5f9013d0d
gabyakd2 <gabrielsal00@hotmail.com>
2023-01-25 18:12:29 -0300

    cambios en home

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
 import "./carousel.css";
 import img1 from '../../imagenes/imgcarr1.png'
 import img2 from '../../imagenes/imgcarr2.png'
@@ -7,40 +8,36 @@ import img4 from '../../imagenes/imgcarr4.png'
 
 const SectionCarrusel = () => {
   return (
-    <div className="countainerPriCarrousel">
-      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src={img1} className="d-block w-100 imgCarrousel" alt="imgcarrusel" />
-    </div>
-    <div className="carousel-item">
-      <img src={img2} className="d-block w-100 imgCarrousel" alt="imgcarrusel" />
-    </div>
-    <div className="carousel-item">
-      <img src={img3} className="d-block w-100 imgCarrousel" alt="imgcarrusel" />
-    </div>
-    <div className="carousel-item">
-      <img src={img4} className="d-block w-100 imgCarrousel" alt="imgcarrusel" />
-    </div>
-  </div>
-  <div className="buttonsCarousel">
-    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Previous</span>
-    </button>
-    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Next</span>
-    </button>  
-  </div>
-</div>
-    </div>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="w-100 h-100 imgCarrousel"
+          src={img1}
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="w-100 h-100 imgCarrousel"
+          src={img2}
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="w-100 h-100 imgCarrousel"
+          src={img3}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="w-100 h-100 imgCarrousel"
+          src={img4}
+          alt="Four slide"
+        />
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import "./home.css";
+import style from "./home.module.css";
+import { Col, Container, Row } from "react-bootstrap";
+import SectionCarrusel from "../Carrusel/Carrusel";
+import trackhome from '../../imagenes/trackpivrhome.png'
+import exthome from '../../imagenes/pivrexthome.png'
 
 export default function Home() {
   useEffect(() => {
@@ -10,25 +14,17 @@ export default function Home() {
 
   return (
     <div>
-      <div className="containerPri">
+      <div className={style.containerPri}>
         <div
-          className="containerTitle"
+          className={style.containerTitle}
           data-aos="zoom-up"
           data-aos-duration="4000"
         >
-          <h1 className="h1Home">PI - VR ARGENTINA</h1>
+          <h1 className={style.h1Home}>PI - VR ARGENTINA</h1>
         </div>
-        <div className="container mt-5 pt-2 bg-dark" id="divContenedor">
-          {/* <img
-            src={imgback}
-            alt="logoHome"
-            className="img-fluid mt-3"
-            id="imgHome"
-          /> */}
-          <div className="espacio" />
-          {/* <SectionCarrusel /> */}
+        {/* <div className="container mt-5 pt-2" id="divContenedor">
           <div data-aos="fade-up" data-aos-duration="1000" className="containerHomeQe">
-            <h3 className="subtitleHome mt-5 mb-3">¿ Qué es Pi-VR ?</h3>
+            <h3 className="subtitleHome mt-3 mb-5">¿ Qué es Pi-VR ?</h3>
             <p className="parrafoHome">
               Somos un emprendimiento enfocado a la venta de full body tracking
               derivados a “SlimeVR” con un año de experiencia en el mismo y con
@@ -48,7 +44,53 @@ export default function Home() {
               ofrecemos asistencia técnica a través de mensaje, llamada o VRC.
             </p>
           </div>
-        </div>
+        </div> */}
+        <Container className="mt-5">
+          <Row >
+            <Col md={6} className='mt-5'>
+              <h3 className={`${style.subtitleHome} mb-3`}>¿Quienes somos?</h3>
+              <p className={style.parrafoHome}>
+              Somos un emprendimiento enfocado a la venta de full body tracking derivados a “SlimeVR” con un año de experiencia en el mismo y con el objetivo de traer esta tecnología a un precio accesible a la Argentina. No requiere de estaciones base y funciona con cualquier headset el cual esté conectado a SteamVR.
+            </p>
+            </Col>
+
+            <Col md={6}>
+              <SectionCarrusel />
+            </Col>
+          </Row>
+        </Container>
+
+        <Container className={`${style.containerFb} mt-5`}>
+        <h3 className={`${style.subtitleHome} mb-3`}>Full-Body</h3>
+          <Row >
+            <Col md={6}>
+              <img src={trackhome} alt="Img Tracker" className={`${style.imgtack} img-fluid`}/>
+            </Col>
+
+            <Col md={6} className='mt-2'>
+              <h3 className={`${style.subtitleHome} mb-3 mt-3`}>Tracker</h3>
+              <p className={style.parrafoHome}>
+              El fullbody tracking PI-VR trabaja en conjunto con el software de SlimeVR a la par que SteamVR; con un diseño cómodo y ajustable tanto para la cintura como para las piernas el cual funciona con cualquier tipo de headset. Uno de los beneficios de este sistema de trackeo es que este mismo no será obstruido por objetos que crucen cerca permitiendo de esta forma usarlo por ejemplo bajo la comodidad de estar bajo las sábanas. También como añadido ofrecemos asistencia técnica a través de mensaje, llamada o VRC.
+            </p>
+            </Col>
+          </Row>
+        </Container>
+
+        <Container className={`${style.containerFb} mt-5`}>
+          <Row >
+            <Col md={6} className='mt-5'>
+              <h3 className={`${style.subtitleHome} mb-3 mt-5`}>Extensor</h3>
+              <p className={style.parrafoHome}>
+              Un extensor es utilizado para mover los pies y también puede ser usado como reemplazo de un tracker, estos tienen que estar conectados a un tracker para funcionar
+            </p>
+            </Col>
+
+            <Col md={6}>
+              <img src={exthome} alt="Img extensor" className={`${style.subtitleHome} mb-3 mt-3`} />
+            </Col>
+          </Row>
+        </Container>
+
       </div>
     </div>
   );
