e57961ba72f4f1fd8e441c23e128987658267bcf
gabyakd2 <gabrielsal00@hotmail.com>
2023-01-25 19:06:59 -0300

    seccion de videos en proceso

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,10 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import style from "./home.module.css";
 import { Col, Container, Row } from "react-bootstrap";
+import ReactPlayer from "react-player";
 import SectionCarrusel from "../Carrusel/Carrusel";
-import trackhome from '../../imagenes/trackpivrhome.png'
-import exthome from '../../imagenes/pivrexthome.png'
+import trackhome from "../../imagenes/trackpivrhome.png";
+import exthome from "../../imagenes/pivrexthome.png";
 
 export default function Home() {
   useEffect(() => {
@@ -46,12 +47,16 @@ export default function Home() {
           </div>
         </div> */}
         <Container className="mt-5">
-          <Row >
-            <Col md={6} className='mt-5'>
+          <Row>
+            <Col md={6} className="mt-5">
               <h3 className={`${style.subtitleHome} mb-3`}>¿Quienes somos?</h3>
               <p className={style.parrafoHome}>
-              Somos un emprendimiento enfocado a la venta de full body tracking derivados a “SlimeVR” con un año de experiencia en el mismo y con el objetivo de traer esta tecnología a un precio accesible a la Argentina. No requiere de estaciones base y funciona con cualquier headset el cual esté conectado a SteamVR.
-            </p>
+                Somos un emprendimiento enfocado a la venta de full body
+                tracking derivados a “SlimeVR” con un año de experiencia en el
+                mismo y con el objetivo de traer esta tecnología a un precio
+                accesible a la Argentina. No requiere de estaciones base y
+                funciona con cualquier headset el cual esté conectado a SteamVR.
+              </p>
             </Col>
 
             <Col md={6}>
@@ -61,36 +66,77 @@ export default function Home() {
         </Container>
 
         <Container className={`${style.containerFb} mt-5`}>
-        <h3 className={`${style.subtitleHome} mb-3`}>Full-Body</h3>
-          <Row >
+          <h3 className={`${style.subtitleHome} mb-3`}>Full-Body</h3>
+          <Row>
             <Col md={6}>
-              <img src={trackhome} alt="Img Tracker" className={`${style.imgtack} img-fluid`}/>
+              <img
+                src={trackhome}
+                alt="Img Tracker"
+                className={`${style.imgtack} img-fluid`}
+              />
             </Col>
 
-            <Col md={6} className='mt-2'>
+            <Col md={6} className="mt-2">
               <h3 className={`${style.subtitleHome} mb-3 mt-3`}>Tracker</h3>
               <p className={style.parrafoHome}>
-              El fullbody tracking PI-VR trabaja en conjunto con el software de SlimeVR a la par que SteamVR; con un diseño cómodo y ajustable tanto para la cintura como para las piernas el cual funciona con cualquier tipo de headset. Uno de los beneficios de este sistema de trackeo es que este mismo no será obstruido por objetos que crucen cerca permitiendo de esta forma usarlo por ejemplo bajo la comodidad de estar bajo las sábanas. También como añadido ofrecemos asistencia técnica a través de mensaje, llamada o VRC.
-            </p>
+                El fullbody tracking PI-VR trabaja en conjunto con el software
+                de SlimeVR a la par que SteamVR; con un diseño cómodo y
+                ajustable tanto para la cintura como para las piernas el cual
+                funciona con cualquier tipo de headset. Uno de los beneficios de
+                este sistema de trackeo es que este mismo no será obstruido por
+                objetos que crucen cerca permitiendo de esta forma usarlo por
+                ejemplo bajo la comodidad de estar bajo las sábanas. También
+                como añadido ofrecemos asistencia técnica a través de mensaje,
+                llamada o VRC.
+              </p>
             </Col>
           </Row>
         </Container>
 
         <Container className={`${style.containerFb} mt-5`}>
-          <Row >
-            <Col md={6} className='mt-5'>
+          <Row>
+            <Col md={6} className="mt-5">
               <h3 className={`${style.subtitleHome} mb-3 mt-5`}>Extensor</h3>
               <p className={style.parrafoHome}>
-              Un extensor es utilizado para mover los pies y también puede ser usado como reemplazo de un tracker, estos tienen que estar conectados a un tracker para funcionar
-            </p>
+                Un extensor es utilizado para mover los pies y también puede ser
+                usado como reemplazo de un tracker, estos tienen que estar
+                conectados a un tracker para funcionar
+              </p>
             </Col>
 
             <Col md={6}>
-              <img src={exthome} alt="Img extensor" className={`${style.subtitleHome} mb-3 mt-3`} />
+              <img
+                src={exthome}
+                alt="Img extensor"
+                className={`${style.imgtack} img-fluid`}
+              />
             </Col>
           </Row>
         </Container>
 
+        <Container className={style.containerMedia}>
+          <Row>
+            <Col md={6}>
+              <ReactPlayer
+                width="80%"
+                url="https://www.youtube.com/watch?v=a6c--K_2qcI&ab_channel=PI-VRArgentina"
+                controls
+              />
+              <ReactPlayer
+                width="80%"
+                url="https://www.youtube.com/watch?v=vNvyvMjqg4U&ab_channel=PI-VRArgentina"
+                controls
+              />
+            </Col>
+            <Col md={6}>
+            <ReactPlayer
+                  width="80%"
+                  url="https://www.youtube.com/watch?v=nlGFijJpzY4&ab_channel=PI-VRArgentina"
+                  controls
+                />
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
