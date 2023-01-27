9b236cf54260c057b56df1df73a61ecc4296a52b
gabyakd2 <gabrielsal00@hotmail.com>
2023-01-27 17:05:38 -0300

    animaciones añadidas

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -24,30 +24,7 @@ export default function Home() {
         >
           <h1 className={style.h1Home}>PI - VR ARGENTINA</h1>
         </div>
-        {/* <div className="container mt-5 pt-2" id="divContenedor">
-          <div data-aos="fade-up" data-aos-duration="1000" className="containerHomeQe">
-            <h3 className="subtitleHome mt-3 mb-5">¿ Qué es Pi-VR ?</h3>
-            <p className="parrafoHome">
-              Somos un emprendimiento enfocado a la venta de full body tracking
-              derivados a “SlimeVR” con un año de experiencia en el mismo y con
-              el objetivo de traer esta tecnología a un precio accesible a la
-              Argentina. No requiere de estaciones base y funciona con cualquier
-              headset el cual esté conectado a SteamVR.
-            </p>
-
-            <p className="parrafoHome">
-              El fullbody tracking PI-VR trabaja en conjunto con el software de
-              SlimeVR a la par que SteamVR; con un diseño cómodo y ajustable
-              tanto para la cintura como para las piernas el cual funciona con
-              cualquier tipo de headset. Uno de los beneficios de este sistema
-              de trackeo es que este mismo no será obstruido por objetos que
-              crucen cerca permitiendo de esta forma usarlo por ejemplo bajo la
-              comodidad de estar bajo las sábanas. También como añadido
-              ofrecemos asistencia técnica a través de mensaje, llamada o VRC.
-            </p>
-          </div>
-        </div> */}
-        <Container className="mt-5">
+        <Container className="mt-5" data-aos="zoom-up" data-aos-duration="3000">
           <Row>
             <Col md={6} className="mt-5">
               <h3 className={`${style.subtitleHome} mb-3`}>¿Quienes somos?</h3>
@@ -74,10 +51,11 @@ export default function Home() {
                 src={trackhome}
                 alt="Img Tracker"
                 className={`${style.imgtack} img-fluid`}
+                data-aos="fade-right" data-aos-duration="2000"
               />
             </Col>
 
-            <Col md={6} className="mt-2">
+            <Col md={6} className="mt-2" data-aos="fade-left" data-aos-duration="2000">
               <h3 className={`${style.subtitleHome} mb-3 mt-3`}>Tracker</h3>
               <p className={style.parrafoHome}>
                 El fullbody tracking PI-VR trabaja en conjunto con el software
@@ -96,7 +74,7 @@ export default function Home() {
 
         <Container className={`${style.containerFb} mt-5`}>
           <Row>
-            <Col md={6} className="mt-5">
+            <Col md={6} className="mt-5" data-aos="fade-up-right" data-aos-duration="3500">
               <h3 className={`${style.subtitleHome} mb-3 mt-5`}>Extensor</h3>
               <p className={style.parrafoHome}>
                 Un extensor es utilizado para mover los pies y también puede ser
@@ -110,6 +88,7 @@ export default function Home() {
                 src={exthome}
                 alt="Img extensor"
                 className={`${style.imgtack} img-fluid`}
+                data-aos="fade-up-left"
               />
             </Col>
           </Row>
@@ -159,10 +138,14 @@ export default function Home() {
             </Col>
           </Row>
         </Container>
-        <div className="divider mt-5 py-1 bg-white"></div>
+
+        <div className="container">
+          <div className="divider mt-5 py-1 bg-white"></div>
+        </div>
+
         <Container className={style.containerComp}>
           <Row>
-            <Col md={6}>
+            <Col md={6} data-aos="flip-left">
               <Card className={style.cardFin}>
                 <Card.Body className="d-flex flex-column">
                   <h3 className={`${style.titleCard} mb-3`}>
@@ -177,7 +160,7 @@ export default function Home() {
               </Card>
             </Col>
 
-            <Col md={6}>
+            <Col md={6} data-aos="flip-left">
               <Card className={style.cardFin}>
                 <Card.Body>
                   <h3 className={`${style.titleCard} mb-3`}>
